40a73c2d341bd488f85da77ab06c590fa7c27fbf
= <=>
2024-02-06 12:38:24 -0800

    1.2.1 big

--- src/main/worker-thread.js
+++ src/main/worker-thread.js
@@ -9,13 +9,19 @@ let hold_save = {}
 const { spawn } = require('child_process');
 const childProcess = spawn(path.join(process.cwd(), "src/main/key-logger.exe"));
 
+parentPort.on('message', m => {
+	hold_watch[m] = hold_watch[m] ? 0 : 1;
+})
+
 childProcess.stdout.on('data', (data) => {
-  // console.log(data.toString().split(','));
-	let [key, state, device] = data.toString().split(',');
-	if (key == 189) return parentPort.postMessage('clear'); // pressing '-' clears all running macros 
+	let e = data.toString().split(',')
+	// console.log(e)
+	let key = +e[0];
+	let state = +e[1] ? 0:1;
+	let device = e[2][1] == 'x';
+	if (key == 189 && state == 1) return parentPort.postMessage('clear'); // pressing '-' clears all running macros 
 	if(!device) return; // bot press - ignore
 	if(!hold_watch[key]) return; // key not important - ignore
-	state = state == 0 ? 1:0; //toggle 
 	if(hold_save[key] != state) {
 		hold_save[key] = state;    
 		parentPort.postMessage(JSON.stringify({[key]: state}))
