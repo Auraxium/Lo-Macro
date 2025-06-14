090ac33f9edc2513319440abf2c0c4a1c8d3fd98
Auraxium <clutchmasterman@gmail.com>
2025-06-13 23:32:32 -0700

    ws

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import { useState, useEffect, useRef } from 'react'
 import Home from './Home'
 import { invoke } from "@tauri-apps/api/core";
+import { io } from "socket.io-client";
+
+const ws = io("http://localhost:23239");
+
+// Listen for events from the server
+ws.on("connect", () => {
+		console.log("Connected to WebSocket server", ws);
+});
+
+ws.on("message", (data) => {
+		console.log("Message from server:", data);
+});
 
 function App() {
-	const [view, setView] = useState(<Home />)
+	const [view, setView] = useState('')
 
 	useEffect(() => {
 		window.setView = setView;
@@ -11,8 +23,8 @@ function App() {
 
 	return (
 		<div className="center h-[100svh] w-full">
-			<div className=" text-black">sadads</div>
 			<div className="border w-[800px] h-[80%] overflow-hidden, ">{view}</div>
+			<button onClick={e => ws.emit('createMessage', 'ur gay')} >asdf</button>
 		</div>
 	)
 }
